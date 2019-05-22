1b4f02a11bbad29bcb197c26dd10f5d6b2c0a897
zxk <276442562@qq.com>
2019-05-22 14:39:03 +0800

    pagefull使用和H5C3复习

---
+++ js/index.js
@@ -0,0 +1,29 @@
+/*初始化fullPage*/
+$(function ($) {
+    $('.container').fullpage({
+        verticalCentered: false,
+        sectionsColor: ["#fadd67", "#84a2d4", "#ef674d", "#ffeedd", "#d04759", "#84d9ed", "#8ac060"],
+        /*右侧小图标*/
+        navigation: true,
+        /*页面滚到某一屏的回调函数*/
+        afterLoad:function (link, index){
+            $('.section').eq(index-1).addClass('in');
+        },
+        /*页面huad速度*/
+        scrollingSpeed:1000,
+        /*页面加载结构完成后注册事件*/
+        afterRender: function () {
+            $('.more').on('click', function () {
+                $.fn.fullpage.moveSectionDown();
+            })
+        },
+        /*页面离开事件 index => nextIndex都是序号数从1开始*/
+        onLeave:function(index, nextIndex,direction ){
+            // 设置第二屏到第三屏离开事件
+            if(index == 2 && nextIndex ==3){
+                $('.section').eq(index-1).addClass('out');
+            }
+        }
+    });
+
+});
